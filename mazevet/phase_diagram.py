5e32077a12159ca9cacfe861dac58acd749b172a
astro-pavan <pt426@cam.ac.uk>
2025-01-29 09:32:23 +0000

    Can now read the .dat files and alter their values (no corrections yet).

--- mazevet/phase_diagram.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-
-eos_table = pd.read_table('Tables/aqua_eos_rhot_v1_0.dat', sep=' ', header=0, skiprows=21,
-                          names=['rho', 'T', 'P', 'ad_grad', 's', 'u', 'c', 'mmw', 'x_ion', 'x_d', 'phase'])
-
-print(eos_table)
